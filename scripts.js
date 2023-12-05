4a166416ac188d52b840a71d1153c4646294ab0c
supunHMSM <jayasun-ct19017.stu.kln.ac.lk>
2023-12-05 19:32:23 +0530

    Add the feature-undoAndRedo...

--- scripts.js
+++ scripts.js
@@ -67,3 +67,86 @@ element.addEventListener('click',function(){
 
     })
 }
+
+//Undo Redo
+var editor = document.getElementById('cEditor');
+var contentHistory = [];
+var alignmentHistory = [];
+var selectionHistory = [];
+var currentStep = 0;
+
+function saveState() {
+    contentHistory = contentHistory.slice(0, currentStep + 1);
+    alignmentHistory = alignmentHistory.slice(0, currentStep + 1);
+    selectionHistory = selectionHistory.slice(0, currentStep + 1);
+
+    contentHistory.push(editor.innerHTML);
+    alignmentHistory.push(getTextAlignment());
+    selectionHistory.push(saveSelection());
+    currentStep = contentHistory.length - 1;
+}
+
+function undo() {
+    if (currentStep > 0) {
+        currentStep--;
+        restoreState();
+    }
+}
+
+function redo() {
+    if (currentStep < contentHistory.length - 1) {
+        currentStep++;
+        restoreState();
+    }
+}
+
+function restoreState() {
+    editor.innerHTML = contentHistory[currentStep];
+    setTextAlignment(alignmentHistory[currentStep]);
+    restoreSelection(selectionHistory[currentStep]);
+}
+
+function getTextAlignment() {
+    return getComputedStyle(editor).textAlign;
+}
+
+function setTextAlignment(alignment) {
+    editor.style.textAlign = alignment;
+}
+
+function saveSelection() {
+    var selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+        var range = selection.getRangeAt(0);
+        return {
+            startContainer: range.startContainer,
+            startOffset: range.startOffset,
+            endContainer: range.endContainer,
+            endOffset: range.endOffset
+        };
+    }
+    return null;
+}
+
+
+function restoreSelection(selection) {
+    if (selection) {
+        var range = document.createRange();
+        range.setStart(selection.startContainer, selection.startOffset);
+        range.setEnd(selection.endContainer, selection.endOffset);
+
+
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+
+document.querySelectorAll('#cEditor').forEach(function (element) {
+    element.addEventListener('input', function () {
+        saveState();
+    });
+});
+
+
+saveState();
